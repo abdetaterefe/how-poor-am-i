ad0c3b0dbcf36b3dd4a564e8d13d1698368b4bea
Abdi Terefe <59355292+abdetaterefe@users.noreply.github.com>
2023-09-09 20:48:34 +0300

    abyssinia bank logo

--- src/lib/components/balance-card.svelte
+++ src/lib/components/balance-card.svelte
@@ -6,6 +6,9 @@
 	<div class="mb-4 bg-yellow-50 rounded-lg p-4">
 		<h1 class="text-yellow-950">I have</h1>
 		<h1 class="text-3xl sm:text-6xl font-bold text-yellow-700">{tBalance}</h1>
+		<div class="flex h-10">
+			<img src="/Bank of Abyssinia Logo.png" alt="Bank of Abyssinia Logo" />
+		</div>
 	</div>
 	<div class="bg-yellow-50 rounded-lg p-4">
 		<p class="text-yellow-900 text-sm sm:text-base font-semibold">
