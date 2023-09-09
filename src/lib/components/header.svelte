f5c6e95e1f56d579e756ca72dc4f26023de76ee9
Abdi Terefe <59355292+abdetaterefe@users.noreply.github.com>
2023-09-09 15:09:33 +0000

    header made

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,22 @@
+<nav class="border-b border-gray-200 bg-gray-50">
+	<div class="max-w-screen-xl flex items-center justify-between mx-auto p-4">
+		<a href="/" class="flex items-center">
+			<span class="text-2xl font-semibold">HaPI</span>
+		</a>
+
+		<div class="w-auto">
+			<ul class="flex font-medium space-x-8">
+				<li>
+					<a href="/how" class=" py-2 text-gray-900 hover:text-yellow-700">HOW DOES THIS WORK</a>
+				</li>
+				<li>
+					<a
+						target="_blank"
+						href="https://github.com/abdetaterefe/how-poor-am-i"
+						class=" py-2 text-gray-900 hover:text-yellow-700">GITHUB</a
+					>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
+	import Header from '$lib/components/header.svelte';
 	import '../app.postcss';
 </script>
 
+<Header />
 <slot />
