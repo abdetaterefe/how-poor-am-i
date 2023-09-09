5a4b25d0747ec938a620580c436c6a50e6ec767e
Abdi Terefe <59355292+abdetaterefe@users.noreply.github.com>
2023-09-09 21:03:25 +0300

    made a footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,11 @@
+<footer class="fixed bottom-0 left-0 w-full bg-yellow-600">
+	<div class="py-6 px-4 md:flex md:items-center md:justify-between">
+		<div class="flex justify-center space-x-2">
+			<p class="text-yellow-950">Made with</p>
+			<a class="underline" href="https://kit.svelte.dev">SvelteKit</a>
+		</div>
+		<div class="flex space-x-6 justify-center">
+			<a href="/how">HOW DOES THE WEBSITE WORK?</a>
+		</div>
+	</div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
+	import Footer from '$lib/components/footer.svelte';
 	import Header from '$lib/components/header.svelte';
 	import '../app.postcss';
 </script>
 
 <Header />
 <slot />
+<Footer />
