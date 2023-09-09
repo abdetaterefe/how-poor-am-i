19be15fe9b4e0428d87e1c91530529dfcc84d117
Abdi Terefe <59355292+abdetaterefe@users.noreply.github.com>
2023-09-09 15:42:08 +0000

    tc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import TransactionCard from '$lib/components/transaction-card.svelte';
+
+	export let data;
+</script>
+
+<div class="flex justify-center px-6 py-24 sm:py-32 lg:px-8">
+	<TransactionCard transactions={data.transactions} />
+</div>
