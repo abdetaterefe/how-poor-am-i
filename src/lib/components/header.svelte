139b7be3f54a0adc0de21ca9371ee7c010a2d9a2
Abdi Terefe <59355292+abdetaterefe@users.noreply.github.com>
2023-09-09 20:03:34 +0300

    header dark

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,4 +1,4 @@
-<nav class="border-b border-gray-200 bg-gray-50">
+<nav class="border-b border-yellow-200 bg-yellow-950">
 	<div class="max-w-screen-xl flex items-center justify-between mx-auto p-4">
 		<a href="/" class="flex items-center">
 			<span class="text-2xl font-semibold">HaPI</span>
@@ -7,13 +7,13 @@
 		<div class="w-auto">
 			<ul class="flex font-medium space-x-8">
 				<li>
-					<a href="/how" class=" py-2 text-gray-900 hover:text-yellow-700">HOW DOES THIS WORK</a>
+					<a href="/how" class=" py-2 text-white hover:text-yellow-200">HOW</a>
 				</li>
 				<li>
 					<a
 						target="_blank"
 						href="https://github.com/abdetaterefe/how-poor-am-i"
-						class=" py-2 text-gray-900 hover:text-yellow-700">GITHUB</a
+						class=" py-2 text-white hover:text-yellow-200">GITHUB</a
 					>
 				</li>
 			</ul>
